cab011eaf797941707a655fac9484fff23f8c048
Markus Müller <markus.mueller@apiida.com>
2021-05-26 15:16:14 +0200

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# sarif-to-sonar
-GitHub action to convert SARIF reports to Sonar's proprietary import format
+# Sarif to Sonar
+
+This action converts a SARIF report to a soner issues file.
+
+## Inputs
+
+### `sarif-file`
+
+**Required** The name of the SARIF report 
+
+### `sonar-file`
+
+**Required** The name of the Sonar file 
+
+
+## Example usage
+
+uses: mquadrat/sarif-to-sonar@v1
+with:
+  sarif-file: 'results.sarif'
+  sonar-file: 'results.sonar'
---
+++ index.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core');
+const fs = require('fs')
+
+
+try {
+
+    // Load the SARIF report 
+    const rawData = fs.readFileSync(core.getInput('sarif-file'), 'utf8');
+    const data = JSON.parse(rawData);
+    const engine = data.runs[0].tool.driver.name;
+
+    const sonarIssues = []; 
+
+    // Loop through all of the items 
+    for (const result of data.runs[0].results) {
+
+        const location = result.locations[0].physicalLocation;
+
+        // Transform into Sonar's format 
+        const issue = {
+            engineId: engine, 
+            ruleId: result.ruleId, 
+            severity: 'MINOR', // BLOCKER, CRITICAL, MAJOR, MINOR, INFO
+            type: 'CODE_SMELL', // BUG, VULNERABILITY, CODE_SMELL
+            primaryLocation: {
+                message: result.message.text, 
+                filePath: location.artifactLocation.uri,
+                textRange: {
+                    startLine: location.region.startLine, 
+                    endLine: location.region.endLine, 
+                    startColumn: location.region.startColumn, 
+                    endColumn: location.region.endColumn
+                }
+            }
+        }
+
+        sonarIssues.push(issue);
+
+    }
+    
+    // Save the output file 
+    fs.writeFileSync(core.getInput('sonar-file'), content);
+
+
+} catch (error) {
+    core.setFailed(error.message);
+}
